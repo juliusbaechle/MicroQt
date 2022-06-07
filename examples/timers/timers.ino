cadaac12861d3f89e89e20d2f2c63a9c9503d5e7
Julius Bächle <julius.baechle@yahoo.de>
2022-06-07 10:43:24 +0200

    timers-Example vereinfacht

--- examples/timers/timers.ino
+++ examples/timers/timers.ino
@@ -1,14 +1,6 @@
 #include <MicroQt.h>
 using namespace MicroQt;
 
-void printSingleShot() {
-  Serial.println("Single Shot!");
-}
-
-void printPerioudicTimeout() {
-  Serial.println("Periodic Timeout!");
-}
-
 // Interval in ms passed through constructor
 Timer singleShotTimer (1000);
 Timer periodicTimer;
@@ -17,8 +9,8 @@ void setup() {
   Serial.begin(9600);
 
   // Connect your action to the timeout signal
-  singleShotTimer.sglTimeout.connect(printSingleShot);
-  // A singleshot timer will be executed only once
+  singleShotTimer.sglTimeout.connect([](){ Serial.println("single shot"); });
+  // A singleshot timer will be executed only once (default is periodic)
   singleShotTimer.setSingleShot(true);
   singleShotTimer.start();
 
@@ -26,7 +18,7 @@ void setup() {
   periodicTimer.setIntervalMs(2000);
   // The timer is executed repeatedly, which is the default behaviour
   periodicTimer.setSingleShot(false);
-  periodicTimer.sglTimeout.connect(printPerioudicTimeout);
+  periodicTimer.sglTimeout.connect([](){ Serial.println("periodic timeout"); });
   periodicTimer.start();
 }
 
